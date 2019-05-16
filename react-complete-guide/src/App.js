2c716d4e0150a06ad203318a207ef79874b116d8
Thomas Arsenault <tarsenault@thekennagroup.com>
2019-05-16 10:54:15 -0400

    Section 3 Lecture 48

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,33 +3,72 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: 'Person One'},
-      { name: 'Person Two'}
-    ]
-  }
 
-  //used as an event handler
-  switchNameHandler = () => {
-    // console.log("was clicked");
-    //DON'T DO THIS this.state.persons[0].name = "Changed Person";
+state = {
+  persons: [
+    { name: 'Person One'},
+    { name: 'Person Two'}
+  ]
+}
+
+//used as an event handler
+switchNameHandler = (newName) => {
+  // console.log("was clicked");
+  //DON'T DO THIS this.state.persons[0].name = "Changed Person";
+  this.setState({persons: [
+    {name: newName},
+    {name: 'Changed Person Two'}
+  ]});
+  };
+
+
+  nameChangedHandler = (event) => {
     this.setState({persons: [
-      {name: 'Changed Person'},
-      {name: 'Changed Person Two'}
+      {name: "Input text"},
+      {name: event.target.value}
     ]});
   }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px'
+    };
     return (
       <div className="App">
-        <Person name={this.state.persons[0].name}/>
-        <Person name={this.state.persons[1].name}>Hello</Person>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          click={this.switchNameHandler.bind(this, "Name!")}/>
+        <Person
+          name={this.state.persons[1].name}
+          click={this.switchNameHandler.bind(this, "Name!")}
+          changed={this.nameChangedHandler}>Hello</Person>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler("Name!!")}>Switch Name</button>
       </div>
     );
+          }
 
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is a header'), 'Hello! React apps are cool');
   }
-}
 
 export default App;
+
+// state = {
+//   persons: [
+//     { name: 'Person One'},
+//     { name: 'Person Two'}
+//   ]
+// }
+
+// //used as an event handler
+// switchNameHandler = () => {
+//   // console.log("was clicked");
+//   //DON'T DO THIS this.state.persons[0].name = "Changed Person";
+//   this.setState({persons: [
+//     {name: 'Changed Person'},
+//     {name: 'Changed Person Two'}
+//   ]});
+// }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
-            <p>Name: {props.name}</p>
+        <div className="Person">
+            <p onClick={props.click}>Name: {props.name}</p>
             <p>Children: {props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
         )
 }
