f2b404bb3698ed2d3bbed12957c47c89c5ec1508
Thomas Arsenault <tarsenault@kenna.ca>
2019-06-13 12:23:50 -0400

    Section 7 Lecutre 90

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Person.css'
 
-const person = (props) => {
-    return (
-        <div className="Person">
-            <p onClick={props.click}>Name: {props.name}</p>
-            <p>Children: {props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-        )
+class Person extends Component {
+    render() {
+        return (
+            <div className="Person">
+                <p onClick={this.props.click}>Name: {this.props.name}</p>
+                <p>Children: {this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+            )
+    }
+
 }
 
-export default person;
+export default Person;
